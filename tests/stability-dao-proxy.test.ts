a2b00ab0573cfc39cb502529a6e423cc2d7ff295
JodsMigel <mikhail.koinov.store@gmail.com>
2023-12-02 19:31:01 +0900

    init commit

---
+++ tests/stability-dao-proxy.test.ts
@@ -0,0 +1,48 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address } from "@graphprotocol/graph-ts"
+import { Upgraded } from "../generated/schema"
+import { Upgraded as UpgradedEvent } from "../generated/StabilityDaoProxy/StabilityDaoProxy"
+import { handleUpgraded } from "../src/stability-dao-proxy"
+import { createUpgradedEvent } from "./stability-dao-proxy-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let implementation = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newUpgradedEvent = createUpgradedEvent(implementation)
+    handleUpgraded(newUpgradedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("Upgraded created and stored", () => {
+    assert.entityCount("Upgraded", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "Upgraded",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "implementation",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
