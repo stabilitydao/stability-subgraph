2bcfe7b3a8292c9d55fba503b07bcbdacbb7dcce
devtealeaf <dev0tea0leaf@gmail.com>
2024-05-16 12:10:34 +0300

    upd: vs hold

--- src/Vault.ts
+++ src/Vault.ts
@@ -11,14 +11,7 @@ import {
   addressZero,
   oneEther,
   platformAddress,
-  ZeroBigDecimal,
-  OneHundredBigDecimal,
-  YearBigDecimal,
-  EtherBigDecimal,
-  OneBigDecimal,
 } from "./constants";
-
-import { pow } from "./math";
 
 import {
   Transfer as TransferEvent,
@@ -33,8 +26,6 @@ import { DefiEdgeStrategyABI } from "../generated/templates/FactoryData/DefiEdge
 import { PriceReaderABI as PriceReaderContract } from "../generated/templates/IchiQuickSwapMerklFarmData/PriceReaderABI";
 import { WithdrawAssets as WithdrawAssetsEventOld } from "../generated/templates/deprecatedData/deprecatedABI";
 import { StrategyBaseABI as StrategyContract } from "../generated/templates/StrategyData/StrategyBaseABI";
-import { ERC20UpgradeableABI } from "../generated/templates/FactoryData/ERC20UpgradeableABI";
-import { PlatformABI as PlatformContract } from "../generated/PlatformData/PlatformABI";
 
 export function handleDepositAssets(event: DepositAssetsEvent): void {
   const vault = VaultEntity.load(event.address) as VaultEntity;
@@ -45,6 +36,12 @@ export function handleDepositAssets(event: DepositAssetsEvent): void {
     .concat(":")
     .concat(event.params.account.toHexString());
 
+  const strategyContract = StrategyContract.bind(
+    changetype<Address>(vault.strategy)
+  );
+
+  const assetsAmounts = strategyContract.assetsAmounts();
+
   //===========Put new data to vaultEntity===========//
   const newTVL = vaultContract.tvl().value0;
   vault.tvl = newTVL;
@@ -53,6 +50,10 @@ export function handleDepositAssets(event: DepositAssetsEvent): void {
     const usersList = vault.vaultUsersList;
     usersList.push(_VaultUserId);
     vault.vaultUsersList = usersList;
+  }
+
+  if (!vault.initAssetsAmounts) {
+    vault.initAssetsAmounts = assetsAmounts.value1;
   }
   vault.save();
 
@@ -64,191 +65,11 @@ export function handleDepositAssets(event: DepositAssetsEvent): void {
       .concat(":")
       .concat(event.address.toHexString())
   );
-  //===========Get platform data===========//
-
-  const platformContract = PlatformContract.bind(
-    Address.fromString(platformAddress)
-  );
-  const getPlatformBalance = platformContract.getBalance(event.params.account);
-
-  const tokensAddress = getPlatformBalance.value0;
-
-  const formatedTokensAddress: Bytes[] = [];
-
-  for (let i = 0; i < tokensAddress.length; i++) {
-    formatedTokensAddress.push(changetype<Bytes>(tokensAddress[i]));
-  }
-
-  const prices = getPlatformBalance.value1;
-
-  const assetsPrice = new Map<string, BigInt>();
-
-  for (let i = 0; i < tokensAddress.length; i++) {
-    assetsPrice.set(tokensAddress[i].toString(), prices[i]);
-  }
-
-  //===========Get assets amounts from strategy contract===========//
-  const strategyContract = StrategyContract.bind(
-    changetype<Address>(vault.strategy)
-  );
-  const assetsAmounts = strategyContract.assetsAmounts();
-  const strategyAssets = assetsAmounts.value0;
-  const strategyAssetsAmounts = assetsAmounts.value1;
-
-  //====================== Calculate VS HOLD ======================//
-
-  let holdYearPercentDiff: BigDecimal = ZeroBigDecimal;
-  let holdPercentDiff: BigDecimal = ZeroBigDecimal;
-  let sum: BigDecimal = ZeroBigDecimal;
-  let sharePriceDifference: BigDecimal = ZeroBigDecimal;
-  let daysFromCreation: String = "";
-
-  const holdTokenPresentProportion: String[] = [];
-  const holdTokenAPR: String[] = [];
-  const amounts: String[] = [];
-  const amountsInUSD: String[] = [];
-  const proportions: String[] = [];
-
-  const sharePriceOnCreation: BigDecimal = OneBigDecimal;
-
-  const sharePrice: BigDecimal = BigDecimal.fromString(
-    vault.sharePrice.toString()
-  ).div(EtherBigDecimal);
-
-  if (
-    strategyAssets.length &&
-    strategyAssetsAmounts.length &&
-    assetsPrice &&
-    vault.AssetsPricesOnCreation.length
-  ) {
-    const tokensDecimals: BigInt[] = strategyAssets.map<BigInt>(
-      (asset: Address) => {
-        const tokenContract = ERC20UpgradeableABI.bind(asset);
-        const decimals = tokenContract.decimals();
-        return BigInt.fromI32(decimals);
-      }
-    );
-
-    for (let i = 0; i < strategyAssetsAmounts.length; i++) {
-      let amount: BigDecimal = BigDecimal.fromString(
-        strategyAssetsAmounts[i].toString()
-      ).div(
-        pow(
-          BigDecimal.fromString("10"),
-          BigInt.fromI32(tokensDecimals[i].toI32())
-        )
-      );
-      amounts.push(amount.toString());
-    }
-
-    for (let i = 0; i < amounts.length; i++) {
-      let address = strategyAssets[i];
-
-      const price = assetsPrice.get(address.toString()).toString();
-
-      let amountInUSD: BigDecimal = BigDecimal.fromString(price)
-        .div(EtherBigDecimal)
-        .times(BigDecimal.fromString(amounts[i]));
-
-      amountsInUSD.push(amountInUSD.toString());
-    }
-
-    for (let i = 0; i < amountsInUSD.length; i++) {
-      sum = sum.plus(BigDecimal.fromString(amountsInUSD[i]));
-    }
-
-    for (let i = 0; i < amountsInUSD.length; i++) {
-      if (amountsInUSD[i]) {
-        let proportion = BigDecimal.fromString(amountsInUSD[i])
-          .div(sum)
-          .times(OneHundredBigDecimal);
-        proportions.push(proportion.toString());
-      } else {
-        proportions.push("0");
-      }
-    }
-
-    sharePriceDifference = sharePrice
-      .minus(sharePriceOnCreation)
-      .times(OneHundredBigDecimal);
-
-    const currentTime = event.block.timestamp;
-    const differenceInSeconds = currentTime.minus(vault.created);
-
-    daysFromCreation = differenceInSeconds
-      .div(BigInt.fromI32(60 * 60 * 24))
-      .toString();
-
-    if (BigDecimal.fromString(daysFromCreation).lt(OneBigDecimal)) {
-      daysFromCreation = "1";
-    }
-
-    for (let i = 0; i < strategyAssets.length; i++) {
-      let assetPrice: BigDecimal = BigDecimal.fromString(
-        assetsPrice[strategyAssets[i].toString()].toString()
-      ).div(EtherBigDecimal);
-
-      let assetPriceOnCreation: BigDecimal = BigDecimal.fromString(
-        vault.AssetsPricesOnCreation[i].toString()
-      ).div(EtherBigDecimal);
-
-      const startProportion: BigDecimal = OneBigDecimal.div(
-        OneHundredBigDecimal
-      ).times(BigDecimal.fromString(proportions[i]));
-
-      const proportionPrice: BigDecimal =
-        startProportion.div(assetPriceOnCreation);
-
-      const presentAmount: BigDecimal = proportionPrice.times(assetPrice);
-
-      const priceDifference: BigDecimal = assetPrice
-        .minus(assetPriceOnCreation)
-        .div(assetPriceOnCreation)
-        .times(OneHundredBigDecimal);
-
-      const percentDiff: BigDecimal =
-        sharePriceDifference.minus(priceDifference);
-
-      let yearPercentDiff = percentDiff
-        .div(BigDecimal.fromString(daysFromCreation))
-        .times(YearBigDecimal);
-
-      if (yearPercentDiff.lt(BigDecimal.fromString("-100"))) {
-        yearPercentDiff = BigDecimal.fromString("-99.99");
-      }
-
-      holdTokenPresentProportion.push(presentAmount.toString());
-      holdTokenAPR.push(yearPercentDiff.toString());
-    }
-
-    const holdPrice = holdTokenPresentProportion.reduce(
-      (acc: BigDecimal, cur: String) => acc.plus(BigDecimal.fromString(cur)),
-      ZeroBigDecimal
-    );
-
-    const priceDifference: BigDecimal = holdPrice
-      .minus(sharePriceOnCreation)
-      .times(OneHundredBigDecimal);
-
-    holdPercentDiff = sharePriceDifference.minus(priceDifference);
-
-    holdYearPercentDiff = holdPercentDiff
-      .div(BigDecimal.fromString(daysFromCreation))
-      .times(YearBigDecimal);
-
-    if (holdYearPercentDiff.lt(BigDecimal.fromString("-100"))) {
-      holdYearPercentDiff = BigDecimal.fromString("-99.99");
-    }
-  }
-
   // ============================================ //
   vaultHistoryEntity.address = event.address;
   vaultHistoryEntity.sharePrice = vault.sharePrice;
   vaultHistoryEntity.TVL = vault.tvl;
   vaultHistoryEntity.timestamp = event.block.timestamp;
-  vaultHistoryEntity.daysFromCreation = daysFromCreation;
-  vaultHistoryEntity.vsHoldAPR = holdYearPercentDiff.toString();
-  vaultHistoryEntity.tokensVsHoldAPR = holdTokenAPR;
   vaultHistoryEntity.save();
 
   //===========vaultHistoryEntity(VaultEntity)===========//
